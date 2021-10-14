0218f0968fed895feb65594e3d1afaad3c18dd80
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-10-14 12:55:07 -0400

    Added a lot of frontend additions
    -Added dashboard and profile views
    - Ability to add and update Profiles
    - Add experience and education
    - Show experience and education

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+// import spinner from "./spinner.gif";
+
+function Spinner() {
+  return (
+    <Fragment>
+      <i className="fa fa-spinner fa-spin fa-4x"></i>
+    </Fragment>
+  );
+}
+
+export default Spinner;
